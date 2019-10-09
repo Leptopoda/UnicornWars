cc17ed0b34e53a45cedc104e6c9e6b1bf1dd31e6
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2019-10-09 22:20:15 +0200

    code formatation

--- 2/Scoreboardtest.php
+++ 2/Scoreboardtest.php
@@ -1,77 +1,68 @@
-
-
 <!DOCTYPE html>
+
 <html>
-<head>
+    <head>
+        <title> scoreboard </title>
+        <meta charset="ISO-8859-1">
+        <link rel="stylesheet" type="text/css" href="eintragen.css">
+    </head>
 
-<title> scoreboard </title>
-<meta charset="ISO-8859-1">
-<link rel="stylesheet" type="text/css" href="eintragen.css">
+    <body>
+        <div id='divplatzt'>
+            <img src="platz.png" alt="platz_schrift">
+        </div>
+        <div id='divnamet'>
+            <img src="name.png" alt="name_schrift">
+        </div>
+        <div id='divscoret'>
+            <img src="score.png" alt="score_schrift">
+        </div>
 
-</head>
+        <div id='divplayagain' >
+            <a href="game.php"><img src="playagain.png" alt="playagain_schrift" /></a>
+        </div>
 
-<body>
+        <?php
+            define('host','localhost');
+            define('user','admin');
+            define('pass','admin');
+            define('db','scoreboard');
+            
+            $con = mysqli_connect(host, user, pass, db);
+                
+                if(!$con) {
+                
+                    echo "Keine verbindung zur DB";
+                
+                }
+                
+                $sql = "SELECT * FROM scoreboard_daten ORDER BY Punkte desc";
+                
+                $ergebnis = $con->query($sql);
+                
+                if($ergebnis->num_rows > 0){
+                    
+                    echo "<table><tr></tr>";
+                    
+                    $Platzierung = 1;
+                    while($row=$ergebnis->fetch_assoc()){
+                        
+                        echo "<tr>";
+                        echo "<td>" . $Platzierung . "<a></a>" ."</td>";
+                        echo "<td>" . $row['Name'] . "</td>";
+                        echo "<td>" . $row['Punkte'] . "</td>";
+                        echo "</tr>";
+                        $Platzierung++;
+                    }
+                    
+                    echo "</table>";
+                    }
+                else{
+                    echo"nix";
+                }
+                
+                $con->close();
+        ?>
+    </body>
 
-<div id='divplatzt'>
-	<img src="platz.png" alt="platz_schrift">
-</div>
-<div id='divnamet'>
-	<img src="name.png" alt="name_schrift">
-</div>
-<div id='divscoret'>
-	<img src="score.png" alt="score_schrift">
-</div>
-
-<div id='divplayagain' >
-	<a href="game.php"><img src="playagain.png" alt="playagain_schrift" /></a>
-</div>
-
-<?php
-	define('host','localhost');
-	define('user','admin');
-	define('pass','admin');
-	define('db','scoreboard');
-	
-	$con = mysqli_connect(host, user, pass, db);
-		
-		if(!$con) {
-		
-			echo "Keine verbindung zur DB";
-		
-		}
-		
-		$sql = "SELECT * FROM scoreboard_daten ORDER BY Punkte desc";
-		
-		$ergebnis = $con->query($sql);
-		
-		if($ergebnis->num_rows > 0){
-			
-			echo "<table><tr></tr>";
-			
-			$Platzierung = 1;
-			while($row=$ergebnis->fetch_assoc()){
-				
-				echo "<tr>";
-				echo "<td>" . $Platzierung . "<a></a>" ."</td>";
-				echo "<td>" . $row['Name'] . "</td>";
-				echo "<td>" . $row['Punkte'] . "</td>";
-				echo "</tr>";
-				$Platzierung++;
-			}
-			
-			echo "</table>";
-			}
-		else{
-			echo"nix";
-		}
-		
-		$con->close();
-		
-
-?>
-
-
-
-
-</body>
 </html>
--- 2/TestScoreEintrag.php
+++ 2/TestScoreEintrag.php
@@ -1,73 +1,58 @@
 <!Doctype html>
 
 <html>
-<head>
+    <head>
+        <title> Name bidde</title>
+        <meta charset="ISO-8859-1">
+        <link rel="stylesheet" type="text/css" href="eintragen.css">
+    </head>
 
-<title> Name bidde</title>
-<meta charset="ISO-8859-1">
-<link rel="stylesheet" type="text/css" href="eintragen.css">
-   
-</head>
+    
+    <body>
+        <?php
+            @$dauer = $_POST['dauer'];
+        ?>
 
-<body>
+        <div id="divname">
+            <img src="name.png" alt="name_schrift">
+        </div>
+        <div id="divscore">
+            <img src="score.png" alt="score_schrift">
+        </div>
+        <form action="" method="post">
+            <input id='divtextfeldn' type="text" name="Username" maxlength='15' />
 
+                
+            <input id='divtextfelds' readonly="readonly" type="text" name="score" value="<?php echo $dauer; ?>" />
 
+            <button id='buttoneintragen' type="submit" name="submit"><img src="abschicken.png"></button>
+        </form>
+        <div id='divcon'>
+            <a href="Scoreboardtest.php"><img src="scoreboard.png" alt="continue_schrift"></a>
+        </div>
 
-<?php
-	@$dauer = $_POST['dauer'];
-?>
-
-<div id="divname">
-	<img src="name.png" alt="name_schrift">
-</div>
-
-<div id="divscore">
-	<img src="score.png" alt="score_schrift">
-</div>
-<form action="" method="post">
-<input id='divtextfeldn' type="text" name="Username" maxlength='15' />
-
-	
-<input id='divtextfelds' readonly="readonly" type="text" name="score" value="<?php echo $dauer; ?>" />
-
-<button id='buttoneintragen' type="submit" name="submit"><img src="abschicken.png"></button>
-
-</form>
-
-<div id='divcon'>
-	<a href="Scoreboardtest.php"><img src="scoreboard.png" alt="continue_schrift"></a>
-</div>
-
-<?php
+        <?php
+            $db = new mysqli('localhost', 'admin','admin','scoreboard');
 
 
-$db = new mysqli('localhost', 'admin','admin','scoreboard');
+            if(!$db): 
+                echo "Keine verbindung zur DB";
+            endif;
 
 
-if(!$db): 
-	echo "Keine verbindung zur DB";
-endif;
+            if(isset($_POST['submit'])):
+                
+                $Username = $_POST['Username'];
+                $score = $_POST['score'];
+                
 
-
-if(isset($_POST['submit'])):
-	
-	$Username = $_POST['Username'];
-	$score = $_POST['score'];
-	
-
-	
-	$absenden = $db -> prepare("INSERT INTO scoreboard_daten (Name,Punkte) VALUES(?,?)");
-	$absenden->bind_param('ss', $Username, $score);
-	$absenden->execute();
-	
-endif;
-
-?>
-	
-
-
-
-
-
-</body>
+                
+                $absenden = $db -> prepare("INSERT INTO scoreboard_daten (Name,Punkte) VALUES(?,?)");
+                $absenden->bind_param('ss', $Username, $score);
+                $absenden->execute();
+                
+            endif;
+        ?>
+    </body>
+    
 </html>
