564672b6bd70900299d6d26c58a4fa2a7e833c0b
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2020-02-03 09:38:16 +0100

    cleanup

--- site/db.php
+++ site/db.php
@@ -1,20 +1,15 @@
 <?php
 	function queryDB($query){
-		$conDB = connectDB();
+		//$conDB = new mysqli('localhost', 'admin','admin','scoreboard');
+		$conDB = new mysqli('localhost', 'UnicornWars','QYE8qtv5R8aMV6Rf','unicornwars');
+		
+		if ($conDB->connect_error) {
+			die("Connection failed: " . $db->connect_error);
+		}
+		
 		$result = $conDB->query($query);
 		//echo "Query successfull";
 		$conDB->close();
 		return($result);
-	}
-
-	function connectDB(){
-		//$db = new mysqli('localhost', 'admin','admin','scoreboard');
-		$db = new mysqli('localhost', 'UnicornWars','QYE8qtv5R8aMV6Rf','unicornwars');
-
-		if ($db->connect_error) {
-			die("Connection failed: " . $db->connect_error);
-		} else {
-			return $db;
-		}
 	}
 ?>
