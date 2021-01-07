d6f386b16b35557a121070643f01230be9b92e36
Nikolas Rimikis <rimikis.nikolas@gmail.com>
2021-01-07 08:41:42 +0000

    Update public/main/db.php

--- public/main/db.php
+++ public/main/db.php
@@ -2,7 +2,7 @@
 	function queryDB($query){
 		//$conDB = new mysqli('localhost', 'admin','admin','scoreboard');
 		//echo'hello in db.php';
-		$conDB = new mysqli('db', 'UnicornWars','QYE8qtv5R8aMV6Rf','UnicornWars');
+		$conDB = new mysqli('unicornwars_db', 'UnicornWars','QYE8qtv5R8aMV6Rf','UnicornWars');
 		//echo'connectet to db';
 		if ($conDB->connect_error) {
 			die("Connection failed: " . $conDB->connect_error);
