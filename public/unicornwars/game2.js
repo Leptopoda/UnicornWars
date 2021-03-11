c40885a9f03177b119f72f6f4f074523718ef5b5
Leptopoda <rimikis.nikolas@gmail.com>
2021-03-11 09:23:23 +0100

    Revert "some stuff"
    This reverts commit 4afdc2303d1cd6b9a3ebc6197113a0ec558d4a5e.

--- public/unicornwars/game2.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-  lauf();
-}
-
-function lauf(){ //delay artige funktion
-  playerIntervall = setInterval(function(){
-    $( ".lauf").toggle(); //wächselt zwischen den beiden bildern, welche in der classe ein (html) hinterlegt sind (lauf1 und lauf2)
-  }, 200);
-}
