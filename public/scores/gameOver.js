e0ce0f1f5e3b464f6683879db7ac289ea4b18cc6
Leptopoda <rimikis.nikolas@gmail.com>
2022-03-31 16:30:05 +0200

    migrate to CF-Pages
    From now on the hosting will be manages by cloudflare pages and only a single cloud-function will handle the requests. This both reduces the thread surface and increases the maintainability.
    
    From now on the direct url of this project will be https://unicornwars.pages.dev and the old https://unicornwars.leptopoda.workers.dev will 301 redirect to the main domain https://unicornwars.rimikis.de.
    
    There shouldn't be any user facing changes and the game will still be accessible under the main domain.

--- public/scores/gameOver.js
+++ public/scores/gameOver.js
@@ -1,35 +1,25 @@
 window.onload = function () {
     score = sessionStorage.getItem("score");
     username = sessionStorage.getItem("username");
-    if (score == null){
+    if (score == null) {
         score = "Bitte erst spielen";
         document.getElementById("submit").style.visibility = 'hidden';
     }
-    if (username !== null){
+    if (username !== null) {
         document.getElementById("username").innerHTML = username;
     }
     document.getElementById("score").innerHTML = score;
 }
 
-async function submit(){
+async function submit() {
     score = sessionStorage.getItem("score");
     username = document.getElementById("username").value
-    
-    if (username !== "" && score !== null){
+
+    if (username !== "" && score !== null) {
         sessionStorage.removeItem("score");
         sessionStorage.setItem("username", username);
-        
-        const data = await fetch('scoreboard.json')
-        .then(response => response.json())
-        data.scores = data.scores.concat({ /*id: data.scores.length + 1,*/ username: username, score: score })            
-        await fetch("/", { method: 'PUT', body: JSON.stringify(data) })
+
+        await fetch("/api/scoreboard", { method: 'PUT', body: JSON.stringify({ username: username, score: score }) })
         window.location.href = "scoreboard.html";
-        
-        /*const json = '{"scores":[{"id":1,"username":"System","score":"000"}]}';
-        const data = JSON.parse(json)
-        alert(JSON.stringify(data.scores))
-        data.scores = data.scores.concat({ id: data.scores.length + 1, username: username, score: score })
-        alert(JSON.stringify(data.scores))
-        alert(JSON.stringify(data))*/
     }
 }
